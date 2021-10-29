425a328e9b0653ddc44fa408ffc7ac9d58cf5b72
argetskulblaka <stevendotworswick@live.com>
2021-10-29 16:55:35 +0100

    Got the binary step activation working as a module. Updated all scripts that currently use it.

--- nndesign/network_functions/transfer_functions/binaryStepActivation.py
+++ nndesign/network_functions/transfer_functions/binaryStepActivation.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def binaryStep(dataIn):
     # conditional on data-type
-    if isinstance(dataIn, int):
+    if isinstance(dataIn, (int, float, np.int64)):
         return 1 if dataIn >= 0 else -1
-    
-
+    elif isinstance(dataIn, np.ndarray):
+        return np.array([binaryStep(xi.item()) for xi in dataIn])

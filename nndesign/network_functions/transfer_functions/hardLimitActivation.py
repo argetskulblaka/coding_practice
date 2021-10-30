582f734164c56deabbff49318941ee033258faae
argetskulblaka <stevendotworswick@live.com>
2021-10-30 15:38:44 +0100

    Hard limits activation functions started

---
+++ nndesign/network_functions/transfer_functions/hardLimitActivation.py
@@ -0,0 +1,13 @@
+# Hard limit (binary step) activation functions.
+#
+# hardLim  --- returns 0 if n < 0; otherwise returns 1
+#
+# hardLimS --- returns -1 if n < 0; otherwise returns 1 ((S)ymmetric hard limits)
+
+import numpy as np
+
+def hardLim(dataIn):
+
+
+
+def hardLimS(dataIn):
